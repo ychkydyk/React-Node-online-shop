cc12a9e03d9fa1f8c4a12dd2cb132346263b3dc1
ychkydyk <zavdv1@gmail.com>
2022-11-18 08:09:43 +0300

    Закончил Бэк
    +

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,10 +1,11 @@
 const Router = require('express')
 const router = new Router()
+const userController = require('../controllers/userController')
+const authMiddleware = require('../middleware/authMiddleware')
 
-
-router.post('/registration',)
-router.post('/login',)
-router.get('/auth',)
+router.post('/registration',userController.registration)
+router.post('/login',userController.login)
+router.get('/auth',authMiddleware, userController.check)
 
 
 module.exports = router
